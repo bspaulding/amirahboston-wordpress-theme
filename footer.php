098b75000199407c5d9848e0393d33f034d24073
Bradley J. Spaulding <bradley.spaulding@motingo.com>
2012-11-19 23:16:48 -0500

    Added footer.php.

---
+++ footer.php
@@ -0,0 +1,14 @@
+  <footer>
+    <nav id="menu" class="mobile">
+      <ul>
+        <li><a href="/who-we-are">Who We Are</a></li>
+        <li><a href="/what-we-do">What We Do</a></li>
+        <li><a href="/resources">Resources</a></li>
+        <li><a href="/contact">Contact</a></li>
+        <li><a href="/donate">Donate</a></li>
+      </ul>
+    </nav>
+    Amirah, Inc. | PO Box 760867 | Melrose, MA 02176 | 781-462-1758 | <a href="mailto:info@amirahboston.org">info@amirahboston.org</a>
+  </footer>
+</body>
+</html>
