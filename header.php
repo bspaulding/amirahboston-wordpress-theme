f8d0d4df03fa515d4ae88796890042f5df51a2f0
Bradley J. Spaulding <bradley.spaulding@motingo.com>
2012-11-19 23:16:21 -0500

    Added header.php.

---
+++ header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Amirah Boston</title>
+  <meta name="viewport" content="width=device-width,user-scalable=no,initial-scale=1.0,maximum-scale=1.0"/>
+  <link rel="stylesheet" type="text/css" href="<? bloginfo('template_url'); ?>/stylesheets/html5reset-1.6.1.css">
+  <link rel="stylesheet" type="text/css" href="<? bloginfo('template_url'); ?>/stylesheets/styles.css">
+
+  <!-- TypeKit -->
+  <script type="text/javascript" src="http://use.typekit.com/tbe5ivf.js"></script>
+  <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+
+  <script src="<? bloginfo('template_url'); ?>/javascripts/jquery.min.js"></script>
+  <script src="<? bloginfo('template_url'); ?>/javascripts/swipe.min.js"></script>
+  <script src="<? bloginfo('template_url'); ?>/javascripts/swipe-init.js"></script>
+</head>
+<body>
+<div id="header-bg"></div>
+<header>
+  <div id="logo" class="page-curl">
+    <a href="/?ref=logo">
+      <img style="width: 100%;" src="<? bloginfo('template_url') ?>/images/logo/logo-transparent-clipped.png"/>
+    </a>
+  </div>
+  <nav class="mobile">
+    <ul>
+      <li><a href="#menu">Menu</a></li>
+    </ul>
+  </nav>
+  <?php wp_nav_menu( array( 'theme_location' => 'header-menu', 'container' => 'nav' ) ); ?>
+</header>
