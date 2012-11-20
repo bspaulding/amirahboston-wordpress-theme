7567dd7f216476fa023841e56839dd0a5393c569
Bradley J. Spaulding <bradley.spaulding@motingo.com>
2012-11-19 23:17:31 -0500

    Added sidebar.php.

---
+++ sidebar.php
@@ -0,0 +1,12 @@
+<section id="twitter" class="aside">                                                                                         
+  <h1>Twitter</h1>                                                                                                           
+  <ul id="tweets" class="aside-content">                                                                                                 
+    <!-- <li><img class="avatar" src=""/><p class="message">a twitter message</p></li>                  -->                          
+    <!-- <li><img class="avatar" src=""/><p class="message">another twitter message</p></li>            -->                          
+    <!-- <li><img class="avatar" src=""/><p class="message">something about human trafficking</p></li>  -->                          
+    <!-- <li><img class="avatar" src=""/><p class="message">a mention of @notforsale!</p></li>          -->                          
+
+    <a class="twitter-timeline" href="https://twitter.com/bradspaulding" data-widget-id="270733987037712384">Tweets by @bradspaulding</a>
+    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+  </ul>                                                                                                                      
+</section>
