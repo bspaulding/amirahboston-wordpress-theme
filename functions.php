307678b696859cb1bc260ed7af0d0d873cc4ea1b
Bradley J. Spaulding <bradley.spaulding@motingo.com>
2012-11-22 00:47:19 -0500

    Updated sidebar to support widgets.

--- functions.php
+++ functions.php
@@ -7,4 +7,15 @@ function register_my_menus() {
 }
 
 add_action( 'init', 'register_my_menus' );
+
+function initialize_sidebar_widgets() {
+  register_sidebar(array(
+    'before_widget' => '<section class="aside">',
+    'before_title'  => '<h1>',
+    'after_title'   => '</h1><div class="aside-content">',
+    'after_widget'  => '</div></section>'
+  ));
+}
+
+add_action( 'widgets_init', 'initialize_sidebar_widgets' );
 ?>
--- sidebar.php
+++ sidebar.php
@@ -1,12 +1,22 @@
-<section id="twitter" class="aside">                                                                                         
-  <h1>Twitter</h1>                                                                                                           
-  <ul id="tweets" class="aside-content">                                                                                                 
-    <!-- <li><img class="avatar" src=""/><p class="message">a twitter message</p></li>                  -->                          
-    <!-- <li><img class="avatar" src=""/><p class="message">another twitter message</p></li>            -->                          
-    <!-- <li><img class="avatar" src=""/><p class="message">something about human trafficking</p></li>  -->                          
-    <!-- <li><img class="avatar" src=""/><p class="message">a mention of @notforsale!</p></li>          -->                          
-
-    <a class="twitter-timeline" href="https://twitter.com/bradspaulding" data-widget-id="270733987037712384">Tweets by @bradspaulding</a>
-    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-  </ul>                                                                                                                      
+<? if ( !dynamic_sidebar() ) : ?>
+<section class="aside">
+  <h1>Widget Title</h1>
+  <ul class="aside-content">
+    <li>Item</li>
+    <li>Item</li>
+    <li>Item</li>
+    <li>Item</li>
+    <li>Item</li>
+  </ul>
 </section>
+<section class="aside">
+  <h1>Widget Title</h1>
+  <ul class="aside-content">
+    <li>Item</li>
+    <li>Item</li>
+    <li>Item</li>
+    <li>Item</li>
+    <li>Item</li>
+  </ul>
+</section>
+<? endif; ?>
