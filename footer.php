461fce3bda33468cf70597f7a153d022aee5aa45
Bradley Spaulding <brad.spaulding@gmail.com>
2013-12-03 16:14:31 -0500

    Updated address in footer

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
   <footer>
     <?php wp_nav_menu( array( 'theme_location' => 'header-menu', 'container' => 'nav' ) ); ?>
-    Amirah, Inc. | PO Box 760867 | Melrose, MA 02176 | 781-462-1758 | <a href="mailto:info@amirahboston.org">info@amirahboston.org</a>
+    Amirah, Inc. | PO Box 54 | Wenham, MA 01984 | 781-462-1758 | <a href="mailto:info@amirahboston.org">info@amirahboston.org</a>
   </footer>
 </body>
 </html>
